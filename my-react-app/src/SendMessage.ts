9bf37bf0877be413ca8f80b8a1591c34cf09bf83
Daniel Cufino <dc58@rice.edu>
2024-01-13 19:38:21 -0600

    Refactored to use APIM routing

--- my-react-app/src/SendMessage.ts
+++ my-react-app/src/SendMessage.ts
@@ -1,13 +1,24 @@
 import axios from 'axios';
 
-const API_GATEWAY_URL = 'https://personal-warmup-gateway.azure-api.net/routeMessage';
+const REACT_APP_API_GATEWAY_URL = process.env.REACT_APP_API_GATEWAY_URL || "";
+const REACT_APP_SUBSCRIPTION_KEY = process.env.REACT_APP_SUBSCRIPTION_KEY || "";
 
 export const sendRequestToAzureFunction = async (messageID: number, data: any): Promise<any> => {
-  const payload = JSON.stringify({ messageID, data });
+  const payload = JSON.stringify({ "messageID": messageID, "data": data });
+//   const payload = JSON.stringify(
+//     {
+//         "messageID": 0,
+//         "data": {
+//             "name": "test"
+//         }
+//     }
+//   );
+  console.log(payload);
   try {
-    const response = await axios.post(API_GATEWAY_URL, payload, {
+    const response = await axios.post(REACT_APP_API_GATEWAY_URL, payload, {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': REACT_APP_SUBSCRIPTION_KEY,
         }
       });
     return response.data;
